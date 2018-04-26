4a68f5c9f9218b92c70daeb31bf8870b5cd4d858
Tanner Summers <Tannersummers@gmail.com>
2018-04-25 22:30:53 -0500

    First version of the UI demo of the activity_detail

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -2,18 +2,23 @@ package com.udacity.sandwichclub;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+    private Sandwich sandwich;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +44,7 @@ public class DetailActivity extends AppCompatActivity {
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
 
-        Sandwich sandwich = JsonUtils.parseSandwichJson(this, json);
+        this.sandwich = JsonUtils.parseSandwichJson(this, json);
 
         if (sandwich == null) {
             // Sandwich data unavailable
@@ -63,5 +68,45 @@ public class DetailActivity extends AppCompatActivity {
 
     private void populateUI() {
 
+//        private String mainName;
+//        private List<String> alsoKnownAs = null;
+//        private String placeOfOrigin;
+//        private String description;
+//        private String image;
+//        private List<String> ingredients = null;
+
+//
+//        TextView mName = (TextView) findViewById(R.id.name_tv);
+//        TextView mNameAlt = (TextView) findViewById(R.id.also_known_tv);
+//        TextView mIngredients = (TextView) findViewById(R.id.ingredients_tv);
+//        TextView mOrigin= (TextView) findViewById(R.id.origin_tv);
+//        TextView mDescription = (TextView) findViewById(R.id.description_tv);
+
+
+        ((TextView) findViewById(R.id.name_tv)).setText(sandwich.getMainName());
+        ((TextView) findViewById(R.id.also_known_tv)).setText(listToString(sandwich.getAlsoKnownAs()));
+        ((TextView) findViewById(R.id.ingredients_tv)).setText(listToString(sandwich.getIngredients()));
+        ((TextView) findViewById(R.id.origin_tv)).setText(sandwich.getPlaceOfOrigin());
+        ((TextView) findViewById(R.id.description_tv)).setText(sandwich.getDescription());
+
+    }
+
+    private String listToString(List<String> items)
+    {
+        StringBuilder mBuilder = new StringBuilder();
+
+        for(int i = 0; i < items.size(); i++)
+        {
+            if(i == items.size() - 1) {
+                mBuilder.append(items.get(i));
+            }
+            else
+            {
+                mBuilder.append(items.get(i));
+                mBuilder.append(", ");
+            }
+        }
+
+        return mBuilder.toString();
     }
 }
